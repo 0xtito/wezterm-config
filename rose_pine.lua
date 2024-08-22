f0310b015b56b7de31fdc26b87734aa9a8d34fcd
0xtito <welovethepenguins@gmail.com>
2024-08-22 13:43:49 -0400

    added rose_pine.lua, tab swapping, and cleaned up

---
+++ rose_pine.lua
@@ -0,0 +1,26 @@
+local rp = {}
+
+rp.colors = {
+	base = "#191724",
+	surface = "#1f1d2e",
+	overlay = "#26233a",
+	muted = "#6e6a86",
+	subtle = "#908caa",
+	text = "#e0def4",
+	love = "#eb6f92",
+	gold = "#f6c177",
+	rose = "#ebbcba",
+	pine = "#31748f",
+	foam = "#9ccfd8",
+	iris = "#c4a7e7",
+	highlight_low = "#21202e",
+	highlight_med = "#403d52",
+	highlight_high = "#524f67",
+}
+
+rp.SOLID_LEFT_ARROW = utf8.char(0xe0b6)
+rp.SOLID_RIGHT_ARROW = utf8.char(0xe0b4)
+
+rp.active = true
+
+return rp
--- wezterm.lua
+++ wezterm.lua
@@ -1,70 +1,46 @@
 local wezterm = require("wezterm")
+local rp = require("rose_pine")
 
--- The filled in variant of the < symbol
+-- Constants
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-
--- The filled in variant of the > symbol
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 
+-- Configuration
 local config = wezterm.config_builder()
 
-local leader
+-- Leader key
+config.leader = (wezterm.target_triple == "x86_64-pc-windows-msvc") and { key = ",", mods = "ALT" }
+	-- config.leader = (wezterm.target_triple == "x86_64-pc-windows-msvc") and { key = "", mods = "ALT" }
+	or { key = ",", mods = "SUPER" }
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	leader = { key = ",", mods = "ALT" }
-else
-	leader = { key = ",", mods = "SUPER" }
-end
+-- General settings
+config.force_reverse_video_cursor = true
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.audible_bell = "Disabled"
+config.check_for_updates = false
+config.font = wezterm.font("JetBrains Mono NL")
+config.color_scheme = "iTerm2 Smoooooth"
+config.inactive_pane_hsb = { hue = 1.0, saturation = 1.0, brightness = 1.0 }
+config.font_size = 14.0
+config.launch_menu = {}
+config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.disable_default_key_bindings = true
 
-config = {
-	force_reverse_video_cursor = true,
+-- Colors
+config.colors = {
+	tab_bar = {
+		background = rp.colors.base,
+		new_tab = { bg_color = rp.colors.overlay, fg_color = rp.colors.text },
+	},
+}
 
-	-- Define cursor styles for different modes
-	cursor_blink_ease_in = "Constant",
-	cursor_blink_ease_out = "Constant",
-	audible_bell = "Disabled",
-	check_for_updates = false,
-	font = wezterm.font("JetBrains Mono NL"),
-	color_scheme = "iTerm2 Smoooooth",
-	colors = {
-		tab_bar = {
-			background = "#0b0022",
-			new_tab = {
-				bg_color = "#0b0022",
-				fg_color = "#2b2042",
-			},
-		},
-	},
-	inactive_pane_hsb = {
-		hue = 1.0,
-		saturation = 1.0,
-		brightness = 1.0,
-	},
-	font_size = 14.0,
-	launch_menu = {},
-	window_padding = {
-		left = 2,
-		right = 2,
-		top = 0,
-		bottom = 0,
-	},
-	tab_bar_at_bottom = true,
-	hide_tab_bar_if_only_one_tab = true,
-	use_fancy_tab_bar = false,
-	disable_default_key_bindings = true,
-	-- allow_win32_input_mode = false,
-	-- send_composed_key_when_left_alt_is_pressed = true,
-	-- debug_key_events = true,
-	-- leader = { key = ",", mods = "ALT" },
-	leader = leader,
-	keys = {
-		-- Map Alt+a to send the sequence for Alt+a
-		-- { key = "a", mods = "ALT", action = wezterm.action({ SendString = "\x1ba" }) },
-		-- -- Map Alt+w to send the sequence for Alt+w
-		-- { key = "w", mods = "ALT", action = wezterm.action({ SendString = "\x1bw" }) },
-		-- Map Alt+w to send the sequence for Alt+w
-		-- { key = "w", mods = "ALT", action = wezterm.action({ SendString = "\x1bw" }) },
-		-- Send "ALT-A" to the terminal when pressing CTRL-A, CTRL-A
+-- Key bindings
+local function define_key_bindings()
+	local keys = {
 		{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
 		{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 		{
@@ -82,7 +58,7 @@ config = {
 			key = "!",
 			mods = "LEADER | SHIFT",
 			action = wezterm.action_callback(function(win, pane)
-				local tab, window = pane:move_to_new_tab()
+				pane:move_to_new_tab()
 			end),
 		},
 		{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
@@ -90,98 +66,100 @@ config = {
 		{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
 		{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
 		{ key = "t", mods = "SHIFT|CTRL", action = "ShowTabNavigator" },
-		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
-		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
-		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
-		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
-		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
-		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
-		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
-		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
-		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
 		{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
 		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-
 		{ key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
 		{ key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
 		{ key = "c", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },
 		{ key = "+", mods = "SHIFT|CTRL", action = "IncreaseFontSize" },
 		{ key = "_", mods = "SHIFT|CTRL", action = wezterm.action.DecreaseFontSize },
 		{ key = "=", mods = "LEADER|CTRL", action = "ResetFontSize" },
-		{
-			key = "E",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.PromptInputLine({
-				description = "Enter new name for tab",
-				action = wezterm.action_callback(function(window, pane, line)
-					-- line will be `nil` if they hit escape without entering anything
-					-- An empty string if they just hit enter
-					-- Or the actual line of text they wrote
-					if line then
-						window:active_tab():set_title(line)
-					end
-				end),
-			}),
-		},
-	},
-	set_environment_variables = {},
-}
+	}
 
+	-- Add tab activation keys
+	for i = 1, 9 do
+		table.insert(keys, { key = tostring(i), mods = "LEADER", action = wezterm.action({ ActivateTab = i - 1 }) })
+	end
+
+	-- Add rename tab key
+	table.insert(keys, {
+		key = "E",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	})
+
+	return keys
+end
+
+config.keys = define_key_bindings()
+
+-- Adding MoveTab from: https://wezfurlong.org/wezterm/config/lua/keyassignment/MoveTab.html?h=move
+for i = 1, 8 do
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
+
+-- Event handlers
 wezterm.on("format-window-title", function()
-	local title = "[" .. wezterm.mux.get_active_workspace() .. "]"
-	title = title .. " " .. wezterm.mux.get_domain():name()
-	title = title .. " - $W"
-	return title
+	return string.format("[%s] %s - $W", wezterm.mux.get_active_workspace(), wezterm.mux.get_domain():name())
 end)
 
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = tab.active_pane.title
-	if tab.tab_title and #tab.tab_title > 0 then
-		title = tab.tab_title
-	end
-	if tab.is_active then
-		return {
-			{ Background = { Color = "#0b0022" } },
-			{ Foreground = { Color = "#2b2042" } },
-			{ Text = SOLID_LEFT_ARROW },
-			{ Background = { Color = "#2b2042" } },
-			{ Foreground = { Color = "#A9A6AC" } },
-			{ Text = (tab.tab_index + 1) .. "| " .. title .. " " },
-			{ Background = { Color = "#0b0022" } },
-			{ Foreground = { Color = "#2b2042" } },
-			{ Text = SOLID_RIGHT_ARROW },
+-- NOTE: Args are: tab, tabs, panes, config, hover, max_width
+wezterm.on("format-tab-title", function(tab, _, _, _, _, _)
+	local title = tab.tab_title and #tab.tab_title > 0 and tab.tab_title or tab.active_pane.title
+	local index = tab.tab_index + 1
+	local colors = rp.active and rp.colors
+		or {
+			base = "#0b0022",
+			surface = "#2b2042",
+			overlay = "#1b1032",
+			text = "#A9A6AC",
+			muted = "#66646C",
 		}
-	else
+
+	local function create_tab_title(bg, fg, text_color)
 		return {
-			{ Background = { Color = "#0b0022" } },
-			{ Foreground = { Color = "#1b1032" } },
+			{ Background = { Color = colors.base } },
+			{ Foreground = { Color = bg } },
 			{ Text = SOLID_LEFT_ARROW },
-			{ Background = { Color = "#1b1032" } },
-			{ Foreground = { Color = "#66646C" } },
-			{ Text = (tab.tab_index + 1) .. "| " .. title .. " " },
-			{ Background = { Color = "#0b0022" } },
-			{ Foreground = { Color = "#1b1032" } },
+			{ Background = { Color = bg } },
+			{ Foreground = { Color = text_color } },
+			{ Text = string.format(" %d| %s ", index, title) },
+			{ Background = { Color = colors.base } },
+			{ Foreground = { Color = bg } },
 			{ Text = SOLID_RIGHT_ARROW },
 		}
 	end
+
+	return tab.is_active and create_tab_title(colors.surface, colors.text, colors.text)
+		or create_tab_title(colors.overlay, colors.muted, colors.muted)
 end)
 
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 	window:gui_window():toggle_fullscreen()
 end)
 
--- folke/zen-mode.nvim config
 wezterm.on("user-var-changed", function(window, pane, name, value)
 	local overrides = window:get_config_overrides() or {}
 	if name == "ZEN_MODE" then
 		local incremental = value:find("+")
 		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
+		if incremental then
+			for _ = 1, number_value do
 				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
 			end
 			overrides.enable_tab_bar = false
 		elseif number_value < 0 then
@@ -196,48 +174,34 @@ wezterm.on("user-var-changed", function(window, pane, name, value)
 	window:set_config_overrides(overrides)
 end)
 
+wezterm.on("update-right-status", function(window, pane)
+	-- local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
+	local date = wezterm.strftime("%H:%M:%S")
+	local colors = rp.colors
+
+	window:set_right_status(wezterm.format({
+		{ Background = { Color = colors.base } },
+		-- { Foreground = { Color = colors.rose } },
+		{ Attribute = { Italic = true } },
+		-- { Text = " " .. window:active_workspace() .. " " },
+		-- { Foreground = { Color = colors.subtle } },
+		-- { Text = "│ " },
+		{ Foreground = { Color = colors.rose } },
+		{ Text = date .. " " },
+	}))
+end)
+
+-- Platform-specific configurations
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	-- config.front_end = "Software" -- OpenGL doesn't work quite well with RDP.
-	-- config.term = "" -- Set to empty so FZF works on windows
-	-- "C:\Program Files\PowerShell\7\pwsh.exe"
 	table.insert(config.launch_menu, { label = "PowerShell", args = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" } })
 	config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe", "-l" }
 end
 
-if wezterm.target_triple == "aarch64-apple-darwin" then
-	-- config.font = wezterm.font_with_fallback({
-	-- 	"JetBrains Mono",
-	-- 	"Menlo",
-	-- 	"DejaVu Sans Mono",
-	-- 	"Bitstream Vera Sans Mono",
-	-- 	"Lucida Console",
-	-- 	"Monaco",
-	-- 	"Consolas",
-	-- 	"Courier New",
-	-- })
-end
-
--- NOTE:
+-- Unix domains
 config.unix_domains = {
-	{
-		name = "usual",
-	},
-	{
-		name = "config",
-		-- socket_path = "C:\\Users\\tito\\.local\\share\\wezterm\\config",
-		-- socket_path = "C:/Users/tito/.local/share/wezterm/config",
-	},
-	{
-		name = "godot",
-		socket_path = "C:\\Users\\tito\\.local\\share\\wezterm\\godot",
-		-- socket_path = "C:/Users/tito/.local/share/wezterm/godot",
-	},
+	{ name = "usual" },
+	{ name = "config" },
+	{ name = "godot", socket_path = "C:\\Users\\tito\\.local\\share\\wezterm\\godot" },
 }
-
--- This causes `wezterm` to act as though it was started as
--- `wezterm connect unix` by default, connecting to the unix
--- domain on startup.
--- If you prefer to connect manually, leave out this line.
--- config.default_gui_startup_args = { "connect", "usual" }
 
 return config
